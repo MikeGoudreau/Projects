74ef63a71128a018a90cfdfa62cf1a35da3af7f5
MikeGoudreau <98497572+MikeGoudreau@users.noreply.github.com>
2022-06-06 12:39:50 -0400

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -128,7 +128,7 @@ where dea.continent is not null
 Select *, (TotalNumberVaccinated/Population)*100 as PercentageVaccinated
 From PopvsVac
 
--- Temp Table
+-- Temp Table \/
 
 Drop Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
@@ -171,3 +171,96 @@ where dea.continent is not null
 
 Select *
 From PercentPopulationVaccinated
+
+-- view for 1st query \/
+
+Create View DataonCovid as
+Select Location, Continent, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by Location, Continent, Date, Total_cases, new_cases, total_deaths, Population
+--order by 1,2
+
+
+-- view for 2nd query \/
+
+Create View Likelihoodofdying as
+Select Location, Continent, date, total_cases, total_deaths, (Total_deaths/Total_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where Location like '%states%'
+Where Continent is not null
+Group by Location, Date, Total_cases, Continent, Total_deaths
+--order by 1,2
+
+
+-- view for 3rd query \/
+
+Create View ContractedPercentage as
+Select Location, Continent, Date, total_cases, Population, (Total_cases/Population)*100 as ContractedPercentage
+From PortfolioProject..CovidDeaths
+--#Where Location like '%states%'
+Where continent is not null
+Group by Location, Date, total_cases, Continent, Population
+--order by 1,2
+
+--view for 4th query \/
+
+Create View HighestInfection as
+Select Location, Continent, Population, MAX(total_cases) as HighestInfectionCount, MAX((Total_cases/Population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--#Where Location like '%states%'
+Where continent is not null
+Group by Location, Population, Continent
+--order by PercentPopulationInfected desc
+
+--view for 5th query \/
+
+Create view HighestDeathCount as
+Select Location, Continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--#Where Location like '%states%'
+Where continent is not null
+Group by Location, Continent
+--order by TotalDeathCount desc
+
+--view for 6th query \/
+
+Create View DeathsbyContinent as
+Select Continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--#Where Location like '%states%'
+Where continent is not null
+Group by Continent
+--order by TotalDeathCount desc
+
+-- view for 7th query \/
+
+Create View Globalnumbersperday as
+Select Date, SUM(new_cases) as new_cases, SUM(cast(new_deaths as int)) as new_deaths, SUM(cast(new_deaths as int))/SUM(New_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where Location like '%states%'
+Where Continent is not null
+Group by date
+--order by 1,2
+
+--view for 8th query \/
+
+Create View Globalnumbers as
+Select SUM(new_cases) as new_cases, SUM(cast(new_deaths as int)) as new_deaths, SUM(cast(new_deaths as int))/SUM(New_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where Location like '%states%'
+Where Continent is not null
+--Group by date
+--order by 1,2
+
+--view for 9th query \/
+
+Create View Vaccinationsperday as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.Location, dea.Date) as TotalNumberVaccinated --(TotalNumberVaccinated/Population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVac vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
